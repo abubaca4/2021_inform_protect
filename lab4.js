a15ff6fa1d194f915666fcf800d7e975caa67973
abubaca4 <abubaca4@gmail.com>
2021-06-01 19:08:41 +0300

    lab 4 complete

--- lab4.js
+++ lab4.js
@@ -75,9 +75,32 @@ function cfb(p) {
     return c.slice(1);
 }
 
+function show_gist(taget_div, labels, data) {
+    let barChart = new Chart(taget_div, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: data
+        }
+    });
+}
+
+function get_static(input) {
+    let result = Array(32).fill(0);
+    input.forEach(element => {
+        result[element]++;
+    });
+    return result;
+}
+
+function sanitize_str(text) {
+    return text.toLowerCase().replace('ё', 'е').replace(/[^а-я]/g, '');
+}
+
 function update_result() {
-    let p = p_data.value;
-    let k = k_data.value;
+    let labels = 'а б в г д е ж з и й к л м н о п р с т у ф х ц ч ш щ ъ ы ь э ю я'.split(' ');
+    let p = sanitize_str(p_data.value);
+    let k = sanitize_str(k_data.value);
     let text_f = "";
     let p_bits = to_bits(p);
     text_f += to_chars(p_bits) + '<br>';
@@ -91,10 +114,21 @@ function update_result() {
     text_f += `cbc_bits: ${cbc_bits}` + '<br>';
     text_f += `p_bits ^ cbc_bits: ${count_diff(p_bits, cbc_bits)}` + '<br>';
     text_f += `ofb_bits: ${ofb_bits}` + '<br>';
-    text_f += `p_bits ^ ecb_bits: ${count_diff(p_bits, ofb_bits)}` + '<br>';
+    text_f += `p_bits ^ ofb_bits: ${count_diff(p_bits, ofb_bits)}` + '<br>';
     text_f += `cfb_bits: ${cfb_bits}` + '<br>';
-    text_f += `p_bits ^ cbc_bits: ${count_diff(p_bits, cfb_bits)}`;
+    text_f += `p_bits ^ cfb_bits: ${count_diff(p_bits, cfb_bits)}`;
     document.getElementById("text_result").innerHTML = text_f;
+    console.log(labels);
+    console.log(get_static(p_bits));
+    show_gist(document.getElementById("p_hist"), labels, [{ label: 'P', data: get_static(p_bits), borderColor: 'rgb(25, 118, 210)', backgroundColor: 'rgb(25, 118, 210)' }]);
+    show_gist(document.getElementById("ecb_hist"), labels, [{ label: 'ECB', data: get_static(ecb_bits), borderColor: 'rgb(255, 0, 0)', backgroundColor: 'rgb(255, 0, 0)' }]);
+    show_gist(document.getElementById("cbc_hist"), labels, [{ label: 'CBC', data: get_static(cbc_bits), borderColor: 'rgb(255, 0, 0)', backgroundColor: 'rgb(255, 0, 0)' }]);
+    show_gist(document.getElementById("ofb_hist"), labels, [{ label: 'OFB', data: get_static(ofb_bits), borderColor: 'rgb(255, 0, 0)', backgroundColor: 'rgb(255, 0, 0)' }]);
+    show_gist(document.getElementById("cfb_hist"), labels, [{ label: 'CFB', data: get_static(cfb_bits), borderColor: 'rgb(255, 0, 0)', backgroundColor: 'rgb(255, 0, 0)' }]);
+    show_gist(document.getElementById("p_ecb_hist"), labels, [{ label: 'P', data: get_static(p_bits), borderColor: 'rgb(25, 118, 210)', backgroundColor: 'rgb(25, 118, 210)' }, { label: 'ECB', data: get_static(ecb_bits), borderColor: 'rgb(255, 0, 0)', backgroundColor: 'rgb(255, 0, 0)' }]);
+    show_gist(document.getElementById("p_cbc_hist"), labels, [{ label: 'P', data: get_static(p_bits), borderColor: 'rgb(25, 118, 210)', backgroundColor: 'rgb(25, 118, 210)' }, { label: 'CBC', data: get_static(cbc_bits), borderColor: 'rgb(255, 0, 0)', backgroundColor: 'rgb(255, 0, 0)' }]);
+    show_gist(document.getElementById("p_ofb_hist"), labels, [{ label: 'P', data: get_static(p_bits), borderColor: 'rgb(25, 118, 210)', backgroundColor: 'rgb(25, 118, 210)' }, { label: 'OFB', data: get_static(ofb_bits), borderColor: 'rgb(255, 0, 0)', backgroundColor: 'rgb(255, 0, 0)' }]);
+    show_gist(document.getElementById("p_cfb_hist"), labels, [{ label: 'P', data: get_static(p_bits), borderColor: 'rgb(25, 118, 210)', backgroundColor: 'rgb(25, 118, 210)' }, { label: 'CFB', data: get_static(cfb_bits), borderColor: 'rgb(255, 0, 0)', backgroundColor: 'rgb(255, 0, 0)' }]);
 }
 
 p_data.addEventListener("change", update_result);
